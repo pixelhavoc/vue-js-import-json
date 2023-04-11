8fc90dc7715daffb9fe9c2bf631d5a204387fdc7
pixelhavoc <siddhant8596@gmail.com>
2023-04-11 15:55:57 +0530

    added json file for data fetching

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <h2>Job Details Page</h2>
+    <div v-if="job">
+        <h2>{{ job.title }}</h2>
         <p>The job id is {{ id }}</p>  
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading data...</p>
     </div>
 </template>
 
@@ -9,11 +13,18 @@
 export default {
     data () {
         return {
-            id: this.$route.params.id
+            job: null
         }
     },
 
-    props: ['id']
+    props: ['id'],
+
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+        .then(res => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message))
+    }
 }
 </script>
 
--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -1,23 +1,33 @@
 <template lang="">
     <div>
         <h1>Jobs</h1>
-        <div v-for="job in jobs" v-bind:key="job.id" class="job">
-            <router-link v-bind:to="{ name: 'jobDetails', params: { id: job.id } }">
-                <h2>{{ job.title }}</h2>
-            </router-link>
+        <div v-if="jobs.length">
+            <div v-for="job in jobs" v-bind:key="job.id" class="job">
+                <router-link v-bind:to="{ name: 'jobDetails', params: { id: job.id } }">
+                    <h2>{{ job.title }}</h2>
+                </router-link>
+            </div>
         </div>
+
+        <div v-else>
+            <p>Loading jobs...</p>
+        </div>
+        
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            jobs: [
-                { title: 'Ninja UX Designer', id: 1, details: 'lorem'},
-                { title: 'Ninja UI Developer', id: 2, details: 'lorem'},
-                { title: 'Ninja Vue Developer', id: 3, details: 'lorem'}
-            ]
+            jobs: []
         }
+    },
+
+    mounted() {
+        fetch('http://localhost:3000/jobs')
+        .then(res => res.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
     }
 
 }
